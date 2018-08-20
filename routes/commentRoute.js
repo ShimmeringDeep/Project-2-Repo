31efb685f1b4f936e61aec687b3b6b665ffa14ed
ShimmeringDeep <dprandazzo@gmail.com>
2018-08-19 21:54:30 -0500

    routine

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -5,6 +5,17 @@ module.exports = function (app) {
     app.post("api/comment", function (req, res) {
         db.Comment.create(req.body).then(function (comment) {
             res.json(comment);
-        })
-    })
+        });
+    });
+
+    app.get("/:eventid", function(req, res) {
+        db.Comment.findAll({
+            where: {
+                isGoing : true,
+                include : [db.Event , db.User]
+            }
+        }).then( function (comments){
+            res.json(comments);
+        });
+    });
 };
