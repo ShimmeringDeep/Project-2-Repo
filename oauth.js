86ccc83acb0c6866c223c4a9f817acb8b3d9a1b6
tbsampson <tbsampson@gmail.com>
2018-08-24 22:47:43 -0500

    save before merge

---
+++ oauth.js
@@ -0,0 +1,24 @@
+var ids = {
+    facebook: {
+      clientID: '325649148172616',
+      clientSecret: '1d8e6907996b98c6c01a85220712a689',
+      callbackURL: 'http://127.0.0.1:1337/auth/facebook/callback'
+    },
+    twitter: {
+      consumerKey: '7XSr2AQMuLen87UvwMGpyQEEV',
+      consumerSecret: 'wVoBBd52Ee3il2L7jLQmLVLOvERkyZaDT7sLhFjDU1lHaIhdLY',
+      callbackURL: "http://127.0.0.1:1337/auth/twitter/callback"
+    },
+    github: {
+      clientID: '9de52fb3c733b26b5ac5',
+      clientSecret: '305d444f104365577842e7944673d78481fd50c4',
+      callbackURL: "http://127.0.0.1:1337/auth/github/callback"
+    },
+    google: {
+      clientID: '147601512434-m3ud0e1h6m21icu5bkrlr62ofeufogpm.apps.googleusercontent.com',
+      clientSecret: 'ac9Q5RoV4sFbpIZyv8HIYEJP',
+      callbackURL: 'http://127.0.0.1:1337/auth/google/callback'
+    }
+  };
+  
+  module.exports = ids;
